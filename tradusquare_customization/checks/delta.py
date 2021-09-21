cb59658128f120bb69b51538d6786a3e66cbab9c
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-09-21 06:47:02 +0200

    Update delta.py

--- tradusquare_customization/checks/delta.py
+++ tradusquare_customization/checks/delta.py
@@ -1,4 +1,4 @@
-#   Copyright 2017 Benito Palacios Sanchez (aka pleonex)
+#   Copyright 2021 Tradusquare
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
